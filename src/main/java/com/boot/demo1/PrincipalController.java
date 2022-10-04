f57d4f045c17e50cdcbe45344bb864d7eeb99ed8
KilianAlu <alu2019068@365.stucom.com>
2022-10-04 16:57:34 +0200

    Arreglando todo

---
+++ src/main/java/com/boot/demo1/PrincipalController.java
@@ -0,0 +1,38 @@
+package com.boot.demo1;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RestController
+public class PrincipalController {
+	@RequestMapping(
+			value = "/AboutUs",
+			method = RequestMethod.GET,
+			produces = "application/json"
+			)
+	public ResponseEntity<String> aboutUs() {
+		final HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+		return new ResponseEntity<String>("{\"Test\": \"Somos 3 estudiantes montando un proyecto\"}",
+				httpHeaders,HttpStatus.OK);
+	}
+	@RequestMapping(
+			value = "/Contacto",
+			method = RequestMethod.GET,
+			produces = "application/json"
+			)
+	public ResponseEntity<String> Contacto() {
+		final HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+		return new ResponseEntity<String>("{\"Test\": \"Email: Alu2019068@365.stucom.com \"" + "\n"
+				+ "\"Test\": \"Telefono: 63224576\"}",
+				httpHeaders,HttpStatus.OK);
+	}
+
+
+}
---
+++ src/main/java/com/boot/demo1/ProyectoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.boot.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBackendApplication.class, args);
+	}
+
+}
