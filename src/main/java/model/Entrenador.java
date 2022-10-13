4b6c71b82aa0c1c74fccd232259aef6dd790ce5f
KilianAlu <alu2019068@365.stucom.com>
2022-10-13 17:34:51 +0200

    creacion entity

---
+++ src/main/java/model/Entrenador.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="")
+@NamedQuery(name="Contacto.findAll", query="SELECT * FROM Contacto c")
+public class Entrenador implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idEntrenador;
+	
+	private String usuario;
+	
+	private String apellidos;
+	
+	private String contraseña;
+	
+	private Jugador[] listaJugadores;
+	
+	private Equipos[] listaEquipos;
+	
+	private Jugada[] listaJugadas;
+	
+	private Anotacion[] listaAnotaciones;
+
+}
