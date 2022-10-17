6a78f7d627a7cd9a8c0c7557af480f75f71980ad
KilianAlu <alu2019068@365.stucom.com>
2022-10-17 17:00:44 +0200

    Video 1 de javi

--- recursos/Jugada.java
+++ recursos/Jugada.java
@@ -7,6 +7,9 @@ import javax.persistence.*;
 @Table(name="")
 @NamedQuery(name="Contacto.findAll", query="SELECT * FROM Contacto c")
 public class Jugada  implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	
 	private Jugador[] listaJugador;
 	
