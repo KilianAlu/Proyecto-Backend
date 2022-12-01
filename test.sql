840783bdd858d4634c5c0ee89b7247ec6e78d2c6
KilianAlu <alu2019068@365.stucom.com>
2022-12-01 16:06:43 +0100

    actu tabla

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 01-12-2022 a las 16:03:10
+-- Tiempo de generación: 28-11-2022 a las 15:43:17
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 7.4.29
 
@@ -40,9 +40,29 @@ CREATE TABLE `Foto` (
 --
 
 INSERT INTO `Foto` (`Id`, `Nombre`, `Especializacion`, `Informacion`, `Foto`) VALUES
-(1, 'Alejandro Dorado Casado', 'Front End', 'Gran pasion por la programacion \n y por el arte digital', 'http://10.0.2.2/php/foto_alex.jpg'),
-(2, 'Kilian Herrada Fernandez', 'Back End', 'Interés por el código abierto y por \nmejorar como programador.', 'http://10.0.2.2/php/foto_kilian.jpg'),
-(3, 'Tigé David Ral Ramirez', 'Informe técnico', 'Me gusta expresar mi creatividad \n a través del desarrollo de software.\nY me satisface resolver problemas\n lógicos.', 'http://10.0.2.2/php/foto_tige.jpg');
+(1, 'Alejandro Dorado Casado', 'Front End', 'Gran pasion por la programacion \n y por el arte digital', ''),
+(2, 'Kilian Herrada Fernandez', 'Back End', 'Interés por el código abierto y por \nmejorar como programador.', ''),
+(3, 'Tigé David Ral Ramirez', 'Informe técnico', 'Me gusta expresar mi creatividad \n a través del desarrollo de software.\nY me satisface resolver problemas\n lógicos.', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paginaInicial`
+--
+
+CREATE TABLE `paginaInicial` (
+  `id` int(11) NOT NULL,
+  `telefono` bigint(20) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `Integrantes` varchar(160) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `paginaInicial`
+--
+
+INSERT INTO `paginaInicial` (`id`, `telefono`, `email`, `Integrantes`) VALUES
+(1, 9322312345, 'Alu2019068@Stucom.com', 'Kilian, Alejandro y tigé');
 
 --
 -- Índices para tablas volcadas
@@ -55,6 +75,12 @@ ALTER TABLE `Foto`
   ADD PRIMARY KEY (`Id`);
 
 --
+-- Indices de la tabla `paginaInicial`
+--
+ALTER TABLE `paginaInicial`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -63,6 +89,12 @@ ALTER TABLE `Foto`
 --
 ALTER TABLE `Foto`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `paginaInicial`
+--
+ALTER TABLE `paginaInicial`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
